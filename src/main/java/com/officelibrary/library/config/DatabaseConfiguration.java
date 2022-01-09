e5d2ed3a8112b3c1c2b394b380aeff43bc7dbc7b
y4oi <y4oi@nykredit.dk>
2022-01-09 22:50:46 +0100

    Added database configuration

---
+++ src/main/java/com/officelibrary/library/config/DatabaseConfiguration.java
@@ -0,0 +1,29 @@
+package com.officelibrary.library.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories("com.officelibrary.library.exposure.repository")
+public class DatabaseConfiguration extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private String port;
+
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    @Override
+    protected String getDatabaseName() {
+        return database;
+    }
+
+}

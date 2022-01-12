0eff83ab0843e5d8b3625a19152566fd85085688
y4oi <y4oi@nykredit.dk>
2022-01-12 09:10:19 +0100

    Added new entities

---
+++ src/main/java/com/officelibrary/library/exposure/model/Category.java
@@ -0,0 +1,47 @@
+package com.officelibrary.library.exposure.model;
+
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Category {
+
+    @Id
+    private String id;
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
